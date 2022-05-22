cf0b1de591e9387ddd58a2da7ac6d2474416eb40
Felice Nicolas Barcellona <felicebarcellona1@gmail.com>
2022-05-22 23:59:16 +0200

    add stores module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'stores', loadChildren: () => import('./features/stores/stores.module').then(m => m.StoresModule) },
+  { path: '**', redirectTo: 'stores' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
