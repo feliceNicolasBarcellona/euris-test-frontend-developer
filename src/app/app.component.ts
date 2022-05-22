7b0f3fd8107c3457b674ed33a5f4a84e4338487c
Felice Nicolas Barcellona <felicebarcellona1@gmail.com>
2022-05-22 22:34:51 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'eur-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'euris-test-frontend-developer';
+}
