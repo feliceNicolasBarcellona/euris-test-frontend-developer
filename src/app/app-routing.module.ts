bc2c018c0152a30f6ecde1ab28aab1c60c8bd8ec
Felice Nicolas Barcellona <felicebarcellona1@gmail.com>
2022-05-23 00:08:56 +0200

    add home  module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: 'stores', loadChildren: () => import('./features/stores/stores.module').then(m => m.StoresModule) },
+  { path: 'home', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) },
   { path: '**', redirectTo: 'stores' }
 ];
 
